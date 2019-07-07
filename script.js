71bfe827134dae0c0444c571f1604f240aa0bf13
Trần Tuấn Trường <42940093+truongsofm@users.noreply.github.com>
2019-07-07 20:09:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,76 @@
-"use strict";"object"!=typeof window.CP&&(window.CP={}),window.CP.PenTimer={programNoLongerBeingMonitored:!1,timeOfFirstCallToShouldStopLoop:0,_loopExits:{},_loopTimers:{},START_MONITORING_AFTER:2e3,STOP_ALL_MONITORING_TIMEOUT:5e3,MAX_TIME_IN_LOOP_WO_EXIT:2200,exitedLoop:function(o){this._loopExits[o]=!0},shouldStopLoop:function(o){if(this.programKilledSoStopMonitoring)return!0;if(this.programNoLongerBeingMonitored)return!1;if(this._loopExits[o])return!1;var t=this._getTime();if(0===this.timeOfFirstCallToShouldStopLoop)return this.timeOfFirstCallToShouldStopLoop=t,!1;var i=t-this.timeOfFirstCallToShouldStopLoop;if(i<this.START_MONITORING_AFTER)return!1;if(i>this.STOP_ALL_MONITORING_TIMEOUT)return this.programNoLongerBeingMonitored=!0,!1;try{this._checkOnInfiniteLoop(o,t)}catch(e){return this._sendErrorMessageToEditor(),this.programKilledSoStopMonitoring=!0,!0}return!1},_sendErrorMessageToEditor:function(){try{if(this._shouldPostMessage()){var o={action:"infinite-loop",line:this._findAroundLineNumber()};parent.postMessage(o,"*")}else this._throwAnErrorToStopPen()}catch(t){this._throwAnErrorToStopPen()}},_shouldPostMessage:function(){return document.location.href.match(/boomerang/)},_throwAnErrorToStopPen:function(){throw"We found an infinite loop in your Pen. We've stopped the Pen from running. Please correct it or contact support@codepen.io."},_findAroundLineNumber:function(){var o=new Error,t=0;if(o.stack){var i=o.stack.match(/boomerang\S+:(\d+):\d+/);i&&(t=i[1])}return t},_checkOnInfiniteLoop:function(o,t){if(!this._loopTimers[o])return this._loopTimers[o]=t,!1;if(t-this._loopTimers[o]>this.MAX_TIME_IN_LOOP_WO_EXIT)throw"Infinite Loop found on loop: "+o},_getTime:function(){return+new Date}},window.CP.shouldStopExecution=function(o){var t=window.CP.PenTimer.shouldStopLoop(o);return!0===t&&console.warn("[CodePen]: An infinite loop (or a loop taking too long) was detected, so we stopped its execution. Sorry!"),t},window.CP.exitedLoop=function(o){window.CP.PenTimer.exitedLoop(o)};
+setTimeout(init, 100);
+
+var obox = document.getElementById('box');
+var aImg = obox.getElementsByTagName('img');
+var radius = 300;
+
+function init(notDelay) {
+  for (var i = 0; i < aImg.length; i++) {
+    aImg[i].style.transform = "rotateY(" + (i * (360 / aImg.length)) + "deg) translateZ(" + radius + "px)";
+    aImg[i].style.transition = "transform 1s";
+
+    if(!notDelay) aImg[i].style.transitionDelay = (aImg.length - i) / 4 + "s";
+  }
+}
+var sX, sY, nX, nY, desX = 0,
+    desY = 0,
+    tX = 0,
+    tY = 10,
+    index = 0;
+document.onmousedown = function(e) {
+  clearInterval(obox.timer);
+  e = e || window.event;
+  var sX = e.clientX,
+      sY = e.clientY;
+
+  this.onmousemove = function(e) {
+    e = e || window.event;
+    var nX = e.clientX,
+        nY = e.clientY;
+    desX = nX - sX;
+    desY = nY - sY;
+    tX += desX * 0.1;
+    tY += desY * 0.1;
+    obox.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
+    sX = nX;
+    sY = nY;
+  }
+
+  this.onmouseup = function(e) {
+    this.onmousemove = this.onmouseup = null;
+    obox.timer = setInterval(function() {
+      desX *= 0.95;
+      desY *= 0.95;
+      tX += desX * 0.1;
+      tY += desY * 0.1;
+      obox.style.transform = "rotateX(" + (-tY) + "deg) rotateY(" + (tX) + "deg)";
+      if (Math.abs(desX) < 0.5 && Math.abs(desY) < 0.5) {
+        clearInterval(obox.timer);
+      }
+    }, 13);
+  }
+
+  return false;
+}
+document.onmousewheel = function(e) {
+  e = e || window.event;
+  var d = e.wheelDelta / 20 || -e.detail;
+  // if (d > 0) {
+  //     index -= 20;
+  // } else {
+  //     index += 30;
+  // }
+  // if (index < -1050) {
+  //     index = -1050;
+  // }
+  // document.body.style.perspective = (1000 + index) + "px";
+  radius += d;
+  init(true);
+};
+
+function mousewheel(obj, fn) {
+  document.onmousewheel === null ? obj.onmousewheel : null;
+}
+// function addEvent(obj, eName, fn) {
+// 	obj.attackEvent?obj.attackEvent("on", eName, fn):null;
+// }
